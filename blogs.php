15e925fd38338cdb12f06b8afa2913537b792144
aarav-tech-123 <developer@aaravtech.net>
2026-09-22 17:44:16 -0400

    changes 22-09-2026

--- blogs.php
+++ blogs.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
 // Fetch published blog posts
 $sql = "SELECT ID, post_title, post_content, post_date, post_author,post_name
         FROM wp_posts
-        WHERE post_type='post' AND post_status='publish'
+        WHERE post_status='publish'
         ORDER BY post_date DESC";
 $result = $conn->query($sql);
 
